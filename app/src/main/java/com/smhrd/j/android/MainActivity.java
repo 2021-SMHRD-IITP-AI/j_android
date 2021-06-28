483d23bc9bd2670f4041035bfcbdd2ed9261f793
박종근 <pjgking@naver.com>
2021-06-28 11:18:00 +0900

    텍스트뷰 추가!

--- app/src/main/java/com/smhrd/j/android/MainActivity.java
+++ app/src/main/java/com/smhrd/j/android/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.v("hhd","111");
 
+        tv_hello = findViewById(R.id.tv_hello);
     }
 }
