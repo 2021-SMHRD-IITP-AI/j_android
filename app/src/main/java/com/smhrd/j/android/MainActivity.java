499df9b594a4ba40088cbdaee166e5b712fe161e
박종근 <pjgking@naver.com>
2021-06-29 08:59:13 +0900

    버튼 추가

--- app/src/main/java/com/smhrd/j/android/MainActivity.java
+++ app/src/main/java/com/smhrd/j/android/MainActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv_hw;
+    private Button btn_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         Log.v("tt","123");
 
         tv_hw = findViewById(R.id.tv_hw);
+        btn_test = findViewById(R.id.btn_test);
 
         Log.v("ada","15151515");
         Log.v("ada","15151515");
