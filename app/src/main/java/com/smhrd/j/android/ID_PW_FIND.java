e385e686cb215ca9d58115620d43d5650d4ba1a8
= <=>
2021-07-10 17:28:53 +0900

    아이디/비밀번호찾기2

--- app/src/main/java/com/smhrd/j/android/ID_PW_FIND.java
+++ app/src/main/java/com/smhrd/j/android/ID_PW_FIND.java
@@ -4,14 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ID_PW_FIND extends AppCompatActivity {
 
-    private Button btn_find_id, btn_find_pw;
+    private Button btn_find_id, btn_find_pw, btn_check;
     private EditText edt_find_e, edt_find_h;
+
+
+    private RequestQueue queue;
+    private StringRequest stringRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +43,7 @@ public class ID_PW_FIND extends AppCompatActivity {
 
         btn_find_id=findViewById(R.id.btn_find_id);
         btn_find_pw=findViewById(R.id.btn_find_pw);
+        btn_check=findViewById(R.id.btn_check);
 
 
         btn_find_pw.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +70,64 @@ public class ID_PW_FIND extends AppCompatActivity {
             }
         });
 
+        btn_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sendRequest();
+            }
+        });
+    }
+
+    private void sendRequest() {
+        queue = Volley.newRequestQueue(this);
+        String url = "http://222.102.104.135:3000/Join";
+        stringRequest = new StringRequest(Request.Method.POST,
+                url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.v("resultValue",response);
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String value = jsonObject.getString("check");
+                    Log.v("result", value);
+
+                    //★이메일과 휴대폰번호가 일치하면  아이디 찾아주기★
+
+//                    if(value.equals("true")){
+//                        Intent intent = new Intent(getApplicationContext(), Login.class);
+//                        startActivity(intent);
+//                        Log.v("test","찾았다!");
+//                    }else{
+//                        edt_find_e.setText("");
+//                        edt_find_h.setText("");
+//                        Log.v("test","실패...................");
+//                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email",edt_find_e.getText().toString());
+                params.put("tel",edt_find_h.getText().toString());
+
+                return params;
+            }
+        };
+        queue.add(stringRequest);
+    }
 
     }
-}
