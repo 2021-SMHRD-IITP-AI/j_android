efdd8f1b0408cb30288e1ed963acda2fdfbcf20d
= <=>
2021-07-10 14:35:48 +0900

    맞춤도시락 화면 수정3

---
+++ app/src/main/java/com/smhrd/j/android/ID_PW_FIND.java
@@ -0,0 +1,14 @@
+package com.smhrd.j.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ID_PW_FIND extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_id_pw_find);
+    }
+}
--- app/src/main/java/com/smhrd/j/android/Lunch_box_main.java
+++ app/src/main/java/com/smhrd/j/android/Lunch_box_main.java
@@ -3,20 +3,35 @@ package com.smhrd.j.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class Lunch_box_main extends AppCompatActivity {
-
-    private RadioButton radioButton1, radioButton2, radioButton3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_box_main);
 
-        radioButton1=findViewById(R.id.tbtn_1);
-        radioButton2=findViewById(R.id.tbtn_2);
-        radioButton3=findViewById(R.id.tbtn_3);
+        final RadioGroup group1=(RadioGroup)findViewById(R.id.RadioGroup_1);
+        final RadioGroup group2=(RadioGroup)findViewById(R.id.RadioGroup_2);
+        final RadioGroup group3=(RadioGroup)findViewById(R.id.RadioGroup_3);
+        final RadioGroup group4=(RadioGroup)findViewById(R.id.RadioGroup_4);
+        final RadioGroup group5=(RadioGroup)findViewById(R.id.RadioGroup_5);
+
+        Button tbtn_1=(Button)findViewById(R.id.tbtn_1);
+
+        group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton=(RadioButton)findViewById(checkedId);
+            }
+        });
+
+        //라디오 버튼 클릭시 이벤트(db로 값 넘겨주기)
+
 
 
     }
