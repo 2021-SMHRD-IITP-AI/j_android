d446e098bd3f9a776a13be4ba8ccc3f4cf15277f
smhrd <dmsrud2928@gmail.com>
2021-07-12 16:58:39 +0900

    맞춤도시락 수정(은경)

--- app/src/main/java/com/smhrd/j/android/Lunch_box_main.java
+++ app/src/main/java/com/smhrd/j/android/Lunch_box_main.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -16,6 +17,8 @@ public class Lunch_box_main extends AppCompatActivity {
 
     private RadioButton tbtn_1,tbtn_2,tbtn_3,tbtn_4,tbtn_5,tbtn_6,tbtn_7
             ,tbtn_8,tbtn_9,tbtn_10,tbtn_11,tbtn_12,tbtn_13,tbtn_14,tbtn_15;
+
+    private String result1,result2, result3, result4, result5;
 
 
     @Override
@@ -52,13 +55,13 @@ public class Lunch_box_main extends AppCompatActivity {
 
                 if(checkedId == R.id.tbtn_1){
                     Toast.makeText(getApplicationContext(),"닭가슴살 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_1.getText().toString();
+                    result1 ="닭가슴살";
                 }else if(checkedId ==R.id.tbtn_2){
                     Toast.makeText(getApplicationContext(),"흰살생선 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_2.getText().toString();
+                    result1 ="흰살생선";
                 }else if(checkedId == R.id.tbtn_3){
                     Toast.makeText(getApplicationContext(),"두부 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_3.getText().toString();
+                    result1 ="두부";
                 }else{
                     Toast.makeText(getApplicationContext(),"단백질 식품을 선택해주세요",Toast.LENGTH_SHORT).show();
                 }
@@ -71,13 +74,13 @@ public class Lunch_box_main extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if (checkedId == R.id.tbtn_4){
                     Toast.makeText(getApplicationContext(),"흰쌀밥 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_4.getText().toString();
+                    result2 ="흰쌀밥";
                 }else if(checkedId ==R.id.tbtn_5){
                     Toast.makeText(getApplicationContext(),"잡곡밥 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_5.getText().toString();
+                   result2="잡곡밥";
                 }else if(checkedId == R.id.tbtn_6){
                     Toast.makeText(getApplicationContext(),"곤약밥 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_6.getText().toString();
+                    result2="곤약밥";
                 }else{
                     Toast.makeText(getApplicationContext(),"탄수화물 식품을 선택해주세요",Toast.LENGTH_SHORT).show();
                 }
@@ -90,13 +93,13 @@ public class Lunch_box_main extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if (checkedId == R.id.tbtn_7){
                     Toast.makeText(getApplicationContext(),"녹색채소 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_7.getText().toString();
+                    result3="녹색채소";
                 }else if(checkedId ==R.id.tbtn_8){
                     Toast.makeText(getApplicationContext(),"씨앗류 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_8.getText().toString();
+                    result3="씨앗류";
                 }else if(checkedId == R.id.tbtn_9){
                     Toast.makeText(getApplicationContext(),"견과류 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_9.getText().toString();
+                    result3="견과류";
                 }else{
                     Toast.makeText(getApplicationContext(),"지방 식품을 선택해주세요",Toast.LENGTH_SHORT).show();
                 }
@@ -109,13 +112,13 @@ public class Lunch_box_main extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if (checkedId == R.id.tbtn_10){
                     Toast.makeText(getApplicationContext(),"과일 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_10.getText().toString();
+                    result4="과일";
                 }else if(checkedId ==R.id.tbtn_11){
                     Toast.makeText(getApplicationContext(),"버섯 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_11.getText().toString();
+                    result4="버섯";
                 }else if(checkedId == R.id.tbtn_12){
                     Toast.makeText(getApplicationContext(),"황색채소 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_12.getText().toString();
+                    result4="황색채소";
                 }else{
                     Toast.makeText(getApplicationContext(),"비타민 식품을 선택해주세요",Toast.LENGTH_SHORT).show();
                 }
@@ -128,45 +131,62 @@ public class Lunch_box_main extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if (checkedId == R.id.tbtn_13){
                     Toast.makeText(getApplicationContext(),"치즈 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_13.getText().toString();
+                    result5="치즈";
                 }else if(checkedId ==R.id.tbtn_14){
                     Toast.makeText(getApplicationContext(),"멸치 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_14.getText().toString();
+                    result5="멸치";
                 }else if(checkedId == R.id.tbtn_15){
                     Toast.makeText(getApplicationContext(),"해조류 선택",Toast.LENGTH_SHORT).show();
-//                    btn_j=tbtn_15.getText().toString();
+                    result5="해조류";
                 }else{
                     Toast.makeText(getApplicationContext(),"칼슘 식품을 선택해주세요",Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
+        btn_j.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(result1 != null && result2 != null && result3 != null && result4 != null && result5 != null){
+                    //Toast.makeText(Lunch_box_main.this, (CharSequence) btn_j,Toast.LENGTH_SHORT).show();
+                    Log.v("야", "담기 성공");
+
+                    //버튼 비활성화(else)/영양소 미선택시 팝업
+                    //btn_g.setEnabled(true);
+                   // return;
+
+                }
+
+            }
+        });
+
         //구매하기 버튼
-//        btn_g.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //맞춤 도시락 구매페이지 인텐트 필요
-//
-//                //맞게 담았는지 확인하는 코드 작성하기
-////                if(btn_j != null){
-////                Toast.makeText(Lunch_box_main.this, (CharSequence) btn_j,Toast.LENGTH_SHORT).show();
-//
-//                    //버튼 비활성화(else)/영양소 미선택시 팝업
-////                    btn_g.setEnabled(true);
-////                    return;
-//
-////                }else{
-////                    Toast.makeText(Lunch_box_main.this,"영양소를 선택 해주세요",Toast.LENGTH_SHORT).show();
-//                    //버튼 활성화
-////                    btn_g.setEnabled(false);
-//
-////
-////                }
-//            }
-//
-//
-//
-//        });
+        btn_g.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //맞춤 도시락 구매페이지 인텐트 필요
+
+                //맞게 담았는지 확인하는 코드 작성하기
+                if(result1 != null && result2 != null && result3 != null && result4 != null && result5 != null){
+                //Toast.makeText(Lunch_box_main.this, (CharSequence) btn_j,Toast.LENGTH_SHORT).show();
+                    Log.v("야", "담기 성공");
+
+                    //버튼 비활성화(else)/영양소 미선택시 팝업
+//                    btn_g.setEnabled(true);
+
+                }else{
+                   // Toast.makeText(Lunch_box_main.this,"영양소를 선택 해주세요",Toast.LENGTH_SHORT).show();
+                   // 버튼 활성화
+                    btn_g.setEnabled(false);
+                    Log.v("야","안 담김"+ result1);
+
+
+                }
+            }
+
+
+
+        });
 
 
 
